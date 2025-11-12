78ce22f13547bcbc9f66983d4e888058e6abf67b
Schenze <franquiturri01@gmail.com>
2025-11-12 16:34:15 -0300

    Version funcional

--- script.js
+++ script.js
@@ -2,34 +2,44 @@
 const personajesElemento = document.getElementById('personajes');
 const nombreFiltroElemento = document.getElementById('nombreFiltro');
 const estadoFiltroElemento = document.getElementById('estadoFiltro');
+const especieFiltroElemento = document.getElementById("especieFiltro");
+const generoFiltroElemento = document.getElementById("generoFiltro");
+const tipoFiltroElemento = document.getElementById("tipoFiltro");
 
 // tenemos que crear la funcion que haga el llamado a la api
 
-async function obtenerPersonajes (name, status){
-
+async function obtenerPersonajes (name, status,species,gender,type){
     let url = 'https://rickandmortyapi.com/api/character/';
-    if (name || status){
+    if (name || status || species || gender || type){
         url += '?';
         if(name){
             url += `name=${name}&`;
         }
-
         if(status){
             url += `status=${status}`;
+        }
+        if(species){
+            url += `species=${species}&`;
+        }
+        if(gender){
+            url += `gender=${gender}&`;
+        }else
+        if(type){
+            url += `type=${type}&`;
         }
     }
     const response = await fetch(url);
     const data = await response.json(); 
-
     return data.results;
 }
 
 // la funcion que va a renderizar
 
-async function verPersonajes (name, status) {
+async function verPersonajes (name, status, species, gender, type) {
     
     // Obtener los personajes filtrados
-    const personajesObtenidos = await obtenerPersonajes(name, status);
+    try{
+    const personajesObtenidos = await obtenerPersonajes(name, status,species,gender,type);
     
     personajesElemento.innerHTML = '';
 
@@ -43,18 +53,38 @@ async function verPersonajes (name, status) {
             <h2> ${personaje.name} </h2>
             <p> Status: ${personaje.status} </p>
             <p> Especie: ${personaje.species} </p>
+            <p> Tipo de especie: ${personaje.type} </p>
+            <p> Genero: ${personaje.gender} </p>
         `;
         personajesElemento.appendChild(tarjeta);
     }
-
+    }catch(error){
+            personajesElemento.innerHTML = `
+            <div class="errorMensaje">
+                <p style= "color: white;">Error no hay personajes encontrados</p>
+            </div>
+        `;
+    }
 }
 
 verPersonajes();
 
-personajesElemento.addEventListener('input', () => {
-verPersonajes(personajesElemento.value,estadoFiltroElemento.value);
+nombreFiltroElemento.addEventListener('input', () => {
+verPersonajes(nombreFiltroElemento.value,estadoFiltroElemento.value,especieFiltroElemento.value,generoFiltroElemento.value,tipoFiltroElemento.value);
 });
 
 estadoFiltroElemento.addEventListener('change', () => {
-verPersonajes(personajesElemento.value,estadoFiltroElemento.value);
+verPersonajes(nombreFiltroElemento.value,estadoFiltroElemento.value,especieFiltroElemento.value,generoFiltroElemento.value,tipoFiltroElemento.value);
+});
+
+especieFiltroElemento.addEventListener('change', () => {
+verPersonajes(nombreFiltroElemento.value,estadoFiltroElemento.value,especieFiltroElemento.value,generoFiltroElemento.value,tipoFiltroElemento.value);
+});
+
+generoFiltroElemento.addEventListener('change', () => {
+verPersonajes(nombreFiltroElemento.value,estadoFiltroElemento.value,especieFiltroElemento.value,generoFiltroElemento.value,tipoFiltroElemento.value);
+});
+
+tipoFiltroElemento.addEventListener('change', () => {
+verPersonajes(nombreFiltroElemento.value,estadoFiltroElemento.value,especieFiltroElemento.value,generoFiltroElemento.value,tipoFiltroElemento.value);
 });
